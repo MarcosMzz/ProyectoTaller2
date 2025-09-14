a5cf2881faf793dd596ef0b71126b253372c3729
Marcos <marcossantinomazzanti@gmail.com>
2025-09-14 13:45:10 -0300

    Se creo el login y es funcional

---
+++ ProyectoTaller/FormLogin.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ProyectoTaller
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TBCorreo = new System.Windows.Forms.TextBox();
+            this.TBPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BTNLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TBCorreo
+            // 
+            this.TBCorreo.Location = new System.Drawing.Point(86, 90);
+            this.TBCorreo.Name = "TBCorreo";
+            this.TBCorreo.Size = new System.Drawing.Size(139, 20);
+            this.TBCorreo.TabIndex = 0;
+            // 
+            // TBPass
+            // 
+            this.TBPass.Location = new System.Drawing.Point(86, 192);
+            this.TBPass.Name = "TBPass";
+            this.TBPass.Size = new System.Drawing.Size(139, 20);
+            this.TBPass.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Correo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(86, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contraseña";
+            // 
+            // BTNLogin
+            // 
+            this.BTNLogin.Location = new System.Drawing.Point(90, 274);
+            this.BTNLogin.Name = "BTNLogin";
+            this.BTNLogin.Size = new System.Drawing.Size(112, 67);
+            this.BTNLogin.TabIndex = 4;
+            this.BTNLogin.Text = "Iniciar Sesion";
+            this.BTNLogin.UseVisualStyleBackColor = true;
+            this.BTNLogin.Click += new System.EventHandler(this.Loggearse);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 380);
+            this.Controls.Add(this.BTNLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBPass);
+            this.Controls.Add(this.TBCorreo);
+            this.Name = "FormLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TBCorreo;
+        private System.Windows.Forms.TextBox TBPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BTNLogin;
+    }
+}
---
+++ ProyectoTaller/FormLogin.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace ProyectoTaller
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private bool CamposVacios(params TextBox[] textBoxes)
+        {
+            foreach (TextBox tb in textBoxes)
+            {
+                if (string.IsNullOrWhiteSpace(tb.Text))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void Loggearse(object sender, EventArgs e)
+        {
+            string email = TBCorreo.Text.Trim();
+            string pass = TBPass.Text.Trim();
+
+            if (CamposVacios(TBCorreo, TBPass))
+            {
+                MessageBox.Show("Por favor, complete todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // Connection string usando localhost
+                string conexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Concesionaria;Integrated Security=True";
+
+                using (SqlConnection conn = new SqlConnection(conexion))
+                {
+                    conn.Open();
+
+                    // Consulta SQL segura con parámetros
+                    string query = "SELECT COUNT(*) FROM Usuarios WHERE Email = @Email AND Pass = @Pass";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Email", email);
+                        cmd.Parameters.AddWithValue("@Pass", pass);
+
+                        int resultado = (int)cmd.ExecuteScalar();
+
+                        if (resultado > 0)
+                        {
+                            MessageBox.Show("Login exitoso", "Bienvenido", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            // Abrir FormPrincipal o siguiente paso
+                        }
+                        else
+                        {
+                            MessageBox.Show("Email o contraseña incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar con la base de datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+}
