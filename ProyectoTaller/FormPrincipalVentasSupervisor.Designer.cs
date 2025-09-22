454de32e2c6b1969bc7a95da4879602d44bcba22
Marcos <marcossantinomazzanti@gmail.com>
2025-09-22 12:15:01 -0300

    La DataGrid de ventas supervisor esta vinculada con la base de datos ahora

--- ProyectoTaller/FormPrincipalVentasSupervisor.Designer.cs
+++ ProyectoTaller/FormPrincipalVentasSupervisor.Designer.cs
@@ -28,56 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DGVentasSupervisor = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVentasSupervisor)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // DGVentasSupervisor
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 118);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(944, 546);
-            this.dataGridView1.TabIndex = 0;
+            this.DGVentasSupervisor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVentasSupervisor.Location = new System.Drawing.Point(0, 145);
+            this.DGVentasSupervisor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DGVentasSupervisor.Name = "DGVentasSupervisor";
+            this.DGVentasSupervisor.RowHeadersWidth = 51;
+            this.DGVentasSupervisor.Size = new System.Drawing.Size(1259, 672);
+            this.DGVentasSupervisor.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Location = new System.Drawing.Point(17, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 77);
+            this.button1.Size = new System.Drawing.Size(143, 95);
             this.button1.TabIndex = 1;
             this.button1.Text = "Filtrar Por usuario";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(825, 13);
+            this.button2.Location = new System.Drawing.Point(1100, 16);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 77);
+            this.button2.Size = new System.Drawing.Size(143, 95);
             this.button2.TabIndex = 2;
             this.button2.Text = "Graficar Ventas";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // FormPrincipalVentasSupervisor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 661);
+            this.ClientSize = new System.Drawing.Size(1259, 814);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.DGVentasSupervisor);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormPrincipalVentasSupervisor";
             this.Text = "Ventas Supervisor";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.cargarDatosVentaSup);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVentasSupervisor)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DGVentasSupervisor;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
